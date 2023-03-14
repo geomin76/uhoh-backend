f167bb24c88c742d72f6fe2faf6892202bdcd9cc
geomin76 <geom@vt.edu>
2023-03-14 10:29:30 -0500

    Return data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, json
+from flask import Flask, request, json, jsonify
 from service.neo4j_functions import Neo4JFunctions
 
 app = Flask(__name__)
@@ -11,10 +11,14 @@ def main():
 def stds():
     # requires a list of symptoms [""]
     data = json.loads(request.data)
-    records = Neo4JFunctions.get_stds(data)
+    records = set(Neo4JFunctions.get_stds(data))
+    res = []
     for std in records:
-        print(std.data())
-    return "Data is back!"
+        symptoms, symptoms_length = Neo4JFunctions.symptom_intersection(data, std.data()['s.name'])
+        res.append(
+            (symptoms_length, std.data()['s.name'], list(symptoms))
+        )
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
--- service/neo4j_functions.py
+++ service/neo4j_functions.py
@@ -1,6 +1,10 @@
 from service.neo4j_client import Neo4JClient
+import json
 
 neo4j = Neo4JClient()
+
+with open('./std_data.json', 'r') as f:
+  std_data = json.load(f)
 
 class Neo4JFunctions:
     def get_stds(std_list):
@@ -20,3 +24,9 @@ class Neo4JFunctions:
         result = tx.run(query, std_list=std_list)
         records = list(result)
         return records
+    
+    def symptom_intersection(symptom_list, std_name):
+        std_data_list = std_data[std_name]
+        result = set(symptom_list).intersection(std_data_list)
+        return result, len(result)
+
